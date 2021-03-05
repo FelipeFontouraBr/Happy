e9aaa0fc30a38a1beda9b182a39917e3cfc7d6d9
FelipeFontouraBr <felipefontouramec@gmail.com>
2021-03-05 13:18:14 +0100

    Remove attribution

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,11 +1,6 @@
 const map = L.map('mapid').setView([51.505, -0.09], 13)
 
-L.tileLayer(
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
-    {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }
-).addTo(map)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
 
 L
 .marker([51.5, -0.09])
