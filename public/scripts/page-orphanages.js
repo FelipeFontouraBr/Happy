6fba8f76813c615319e15a72fd5e6699c3adec8c
FelipeFontouraBr <felipefontouramec@gmail.com>
2021-03-08 13:55:46 +0100

    Create icon and popoup

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -5,7 +5,20 @@ const map = L.map('mapid').setView([38.8795955,-6.9802756], 15)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
 
 //create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg", 
+    iconSize: [58, 68],
+    iconAnchor: [29,68],
+    popupAnchor: [170,2]
+})
 
+//create popup overlay
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+})
 
 //create and add marker
 
