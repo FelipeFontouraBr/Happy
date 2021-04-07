2e0356eed9a555046ee273d9022ebc5a7728d516
FelipeFontouraBr <felipefontouramec@gmail.com>
2021-04-07 19:28:05 +0200

    Create fakedata.js

---
+++ src/database/fakedata.js
@@ -0,0 +1,20 @@
+// Vai receber os dados do orfanato
+
+module.exports = [
+    {
+        id: 1,
+        name: "Lar das meninas",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
+        images: [
+            "https://images.unsplash.com/photo-1613244470042-e69e8ccb303a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjE3ODE2Mzkx&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
+            
+            "https://images.unsplash.com/photo-1614270270735-e93b1234fc7c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjE3ODE2NDIy&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"
+        ]
+    },
+    {
+        id: 2,
+        name: "Lar dos meninos",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
+
+    }
+]
