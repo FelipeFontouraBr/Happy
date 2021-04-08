48ef07bcbe30e11e6ce93a169619ecbbc4ebe00d
FelipeFontouraBr <felipefontouramec@gmail.com>
2021-04-08 18:38:18 +0200

    Import data from fakedata.js to orphanages

--- src/pages.js
+++ src/pages.js
@@ -1,5 +1,8 @@
 // Responsável por conter as funções das rotas .get
 
+// importação dos módulos
+const orphanages = require('./database/fakedata.js');
+
 module.exports = { // Essa linha exporta o objeto para o arquivo server.js
 
     index(req, res) {
@@ -9,7 +12,7 @@ module.exports = { // Essa linha exporta o objeto para o arquivo server.js
         return res.render('orphanage')
     },
     orphanages(req, res) {
-        return res.render('orphanages')
+        return res.render('orphanages', { orphanages })
     },
     createOrphanage(req,res) {
         return res.render('create-orphanage')
