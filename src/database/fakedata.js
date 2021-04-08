e5f8feb590675728330cfb7fbc535b5ebbb2551d
FelipeFontouraBr <felipefontouramec@gmail.com>
2021-04-08 13:31:53 +0200

    Update "Home's boy"

--- src/database/fakedata.js
+++ src/database/fakedata.js
@@ -1,5 +1,6 @@
 // Vai receber os dados do orfanato
-
+// criação de um modulo que será exportado
+// um javascript que será um array
 module.exports = [
     {
         id: 1,
@@ -11,12 +12,25 @@ module.exports = [
             "https://images.unsplash.com/photo-1613244470042-e69e8ccb303a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjE3ODE2Mzkx&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
             
             "https://images.unsplash.com/photo-1614270270735-e93b1234fc7c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjE3ODE2NDIy&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"
-        ]
+        ],
+        instructions: "Veja como se sentir a vontade e traga muito amor e paciência para dar",
+        opening_hours: "Horário de visitas das 18h até 8h",
+        open_on_weekends: "1"
     },
+    
     {
         id: 2,
+        lat: "38.8795955",
+        lng: "-6.9702756",
         name: "Lar dos meninos",
         description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
-
+        images: [
+            "https://images.unsplash.com/photo-1613244470042-e69e8ccb303a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjE3ODE2Mzkx&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
+            
+            "https://images.unsplash.com/photo-1614270270735-e93b1234fc7c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjE3ODE2NDIy&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"
+        ],
+        instructions: "Veja como se sentir a vontade e traga muito amor e paciência para dar",
+        opening_hours: "Horário de visitas das 18h até 8h",
+        open_on_weekends: "0"
     }
 ]
