3076938ba781edbf3411afbf0a29fb201f68f6be
FelipeFontouraBr <felipefontouramec@gmail.com>
2021-04-09 20:49:16 +0200

    Added data

--- src/database/test.js
+++ src/database/test.js
@@ -1,12 +1,13 @@
 const Database = require('./db')
 
-Database.then(function(db) {
+Database.then(async (db) => {
     // inserir dados na tabela 
-    db.run(`
+    await db.run(`
         INSERT INTO orphanages (
             lat, 
             lng,
             name,
+            about,
             whatsapp,
             images,
             instructions,
@@ -14,16 +15,17 @@ Database.then(function(db) {
             open_on_weekends
         ) VALUES (
             "38.8795955",
-            "-6.9602756",
-            "Lar das meninas",
+            "-6.9702756",
+            "Lar dos meninos",
             "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
             "https://images.unsplash.com/photo-1613244470042-e69e8ccb303a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjE3ODE2Mzkx&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
             "Veja como se sentir a vontade e traga muito amor e paciência para dar",
             "Horário de visitas das 18h até 8h",
-            "1"
+            "2"
         );
     `)
 
     // consultar dados na tabela
-
+    const selectdOrphanages = await db.all("SELECT * FROM orphanages")
+    console.log(selectdOrphanages)
 })
