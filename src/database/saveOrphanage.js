de23085e97f7145e444ac5a4304d5750ed0781b2
FelipeFontouraBr <felipefontouramec@gmail.com>
2021-04-30 08:28:25 +0200

    Added variables to values

--- src/database/saveOrphanage.js
+++ src/database/saveOrphanage.js
@@ -1,5 +1,5 @@
 function saveOrphanage(db, orphanage) {
-    return db.run(`
+  return db.run(`
     INSERT INTO orphanages (
         lat, 
         lng,
@@ -11,16 +11,17 @@ function saveOrphanage(db, orphanage) {
         opening_hours,
         open_on_weekends
     ) VALUES (
-        "38.8795955",
-        "-6.9702756",
-        "Lar dos meninos",
-        "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
-        "https://images.unsplash.com/photo-1613244470042-e69e8ccb303a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjE3ODE2Mzkx&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
-        "Veja como se sentir a vontade e traga muito amor e paciência para dar",
-        "Horário de visitas das 18h até 8h",
-        "2"
+        "${orphanage.lat}",
+        "${orphanage.lng}",
+        "${orphanage.name}",
+        "${orphanage.about}",
+        "${orphanage.whatsapp}",
+        "${orphanage.images}",
+        "${orphanage.instructions}",
+        "${orphanage.opening_hours}",
+        "${orphanage.open_on_weekends}",
     );
-    `)
+    `);
 }
 
 module.exports = saveOrphanage;
