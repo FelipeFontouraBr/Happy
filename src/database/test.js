98404b9139b411ae8f4635b08006b7e72db7f7a7
FelipeFontouraBr <felipefontouramec@gmail.com>
2021-04-30 08:52:41 +0200

    Fixed error: about and description

--- src/database/test.js
+++ src/database/test.js
@@ -7,9 +7,10 @@ Database.then(async (db) => {
     await saveOrphanage(db, {
             id: 1,
             lat: "38.8795955",
-            lng: "-6.9102756",
-            name: "Lar de amor",
-            description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
+            lng: "-6.9602756",
+            name: "Lar das meninas",
+            about:"Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social",
+            whatsapp:"6550336458",
             images: [
                 "https://images.unsplash.com/photo-1613244470042-e69e8ccb303a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjE3ODE2Mzkx&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
                 
@@ -20,11 +21,14 @@ Database.then(async (db) => {
             open_on_weekends: "1"    
     })
 
-    // consultar dados na tabela
-    const selectdOrphanages = await db.all("SELECT * FROM orphanages")
-    console.log(selectdOrphanages)
+    // // consultar dados na tabela
+    // const selectdOrphanages = await db.all("SELECT * FROM orphanages")
+    // console.log(selectdOrphanages)
 
-    // consultar somente 1 orphanato, pelo id
-    const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "3"')
-    console.log(orphanage)
+    // // consultar somente 1 orphanato, pelo id
+    // const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "3"')
+    // console.log(orphanage)
+
+    // // deletar dado da tabela
+    // console.log(await db.run("DELETE FROM orphanages WHERE id = '4'"))
 })
